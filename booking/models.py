2285abc16b8cdc70f1147fc313060e3e02a15f0a
DelroyGayle <delroygayle01@gmail.com>
2023-11-05 15:14:12 +0000

    Create Models for Employer and Employee. For Testing Purposes

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employer(models.Model):
+    company_name = models.CharField(max_length=45)
+    number_of_employees = models.IntegerField()    
+    employer_test_flag = models.BooleanField(default=False)
+
+
+class Employee(models.Model):
+    TITLES = [
+        ("Mr", "Mister"),
+        ("Ms", "Miss"),
+        ("Mx", "Unspecified"),
+    ]
+    title = models.CharField(max_length=2, choices=TITLES)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)    
+    employee_test_flag = models.BooleanField(default=False)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
