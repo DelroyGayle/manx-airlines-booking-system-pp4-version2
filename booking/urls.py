13933727837e419d52be3d6de9b567215d5e9d8f
DelroyGayle <delroygayle01@gmail.com>
2023-11-06 01:36:17 +0000

    Add 'Create Booking' Functionality. Test that the POST Method worke correctly

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('create/', views.create_booking, name='create-booking'),
+    path('booking/<id>/', views.view_booking, name='view-booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .forms import BookingForm
+from .models import Employer
 
 # Create your views here.
 
@@ -12,5 +15,27 @@ def homepage(request):
 def create_booking(request):
     form = BookingForm()
     context = {'form': form}
-    print(context)
+
+    if request.method == 'POST':
+        company_name = request.POST.get('company_name')
+        number_of_employees = request.POST.get('number_of_employees')
+        # employer_test_flag will either be set to "on" or None
+        # Handle it so that it is either "on" or False
+        employer_test_flag = request.POST.get('employer_test_flag', False)
+        print(company_name, number_of_employees, employer_test_flag)
+
+        new_employer = Employer()
+
+        new_employer.company_name = company_name
+        new_employer.number_of_employees = number_of_employees
+        # new_employer.employer_test_flag = True if employer_test_flag == "on" else False
+        new_employer.employer_test_flag = employer_test_flag
+        new_employer.save()
+
+        return HttpResponseRedirect(reverse("view-booking", kwargs={'id': new_employer.pk}))
+            
     return render(request, 'booking/create-booking.html', context)
+
+
+def view_booking(request, id):
+    return render(request, 'booking/view-booking.html', {})
