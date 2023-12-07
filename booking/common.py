f6cccf16d7830a3e3ca063dec49736171e242cdd
DelroyGayle <delroygayle01@gmail.com>
2023-12-07 11:41:22 +0000

    FIX41 hidden Error

--- booking/common.py
+++ booking/common.py
@@ -47,8 +47,8 @@ class Common:
     the_booking_id = None
     heroku_editmode = None
     the_original_details = None
-    Common.the_hidden = None
-    Common.the_bags_remarks = None
+    the_hidden = None
+    the_bags_remarks = None
 
     # TODO ???
     context_2ndcopy = None
